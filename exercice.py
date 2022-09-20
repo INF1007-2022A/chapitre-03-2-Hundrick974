8ab2e2a39956576f573712c0aea033f660a76325
Hundrick974 <harry.law-yen@polymtl.ca>
2022-09-19 21:08:24 -0400

    Exos finis

--- exercice.py
+++ exercice.py
@@ -1,34 +1,46 @@
 #!/usr/bin/env python
 
-
 def dissipated_power(voltage, resistance):
 	# TODO: Calculer la puissance dissipée par la résistance.
-	return 0
+	p = voltage**2 / resistance
+	return p
 
 def orthogonal(v1, v2):
 	# TODO: Retourner vrai si les vecteurs sont orthogonaux, faux sinon.
-	v1[0] # Pour accéder au X
-	v1[1] # Pour accéder au Y
-	pass
+	#v1[0] # Pour accéder au X
+	#v1[1] # Pour accéder au Y
+	produit_scalaire = v1[0] * v2[0] + v1[1] * v2[1]
+	return produit_scalaire == 0
 
 def average(values):
 	# TODO: Calculer la moyenne des valeurs positives (on ignore les valeurs strictement négatives).
+	compteur = somme = 0
 	for v in values:
-		pass # La variable v contient une valeur de la liste.
+		if v >= 0:
+			compteur += 1
+			somme += v
+
+	moyenne = somme / compteur
+	return moyenne # La variable v contient une valeur de la liste.
 
 def bills(value):
-	# TODO: Calculez le nombre de billets de 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
+	# TODO: Calculez le nombre de billets de 100$, 20$, 10$ et 5$ et pièces de 1$ à remettre pour représenter la valeur.
+	twenties = tens = fives = ones = 0
 	while value != 0:
 		if value >= 20:
-			pass
+			twenties = value // 20
+			value = value % 20
 		elif value >= 10:
-			pass
+			tens = value // 10
+			value = value % 10
 		elif value >= 5:
-			pass
+			fives = value // 5
+			value = value % 5
 		elif value >= 1:
-			pass
+			ones = value
+			value = 0
 
-	return (twenties, tens, fives, twos, ones);
+	return (twenties, tens, fives, ones)
 
 def format_base(value, base, digit_letters):
 	# Formater un nombre dans une base donné en utilisant les lettres fournies pour les chiffres<
@@ -36,11 +48,13 @@ def format_base(value, base, digit_letters):
 	result = ""
 	abs_value = abs(value)
 	while abs_value != 0:
-		pass
+		digit_value = abs_value % base
+		result += digit_letters[digit_value]
+		abs_value //= base
 	if value < 0:
 		# TODO: Ne pas oublier d'ajouter '-' devant pour les nombres négatifs.
-		pass
-	return result
+		result += "-"
+	return result[::-1]
 
 
 if __name__ == "__main__":
